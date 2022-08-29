f736addec2ef7e2690026b7319af4cb4c21907dc
Muhammad Ali <muhammad797@hotmail.com>
2022-08-30 00:59:05 +0500

    Marketplace base contract, deploy

--- src/backend/contracts/Marketplace.sol
+++ src/backend/contracts/Marketplace.sol
@@ -5,5 +5,12 @@ import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 contract Marketplace is ReentrancyGuard {
-  
+    address payable public immutable feeAccount; // account that receives fees
+    uint256 public immutable feePercent; // fee percent on sales
+    uint256 public itemCount;
+
+    constructor(uint256 _feePercent) {
+        feeAccount = payable(msg.sender);
+        feePercent = _feePercent;
+    }
 }
--- src/backend/scripts/deploy.js
+++ src/backend/scripts/deploy.js
@@ -6,12 +6,21 @@ async function main() {
   console.log('Deploying contracts with the account:', deployer.address);
   console.log('Account balance:', (await deployer.getBalance()).toString());
 
-  // deploy contracts here:
+  // deploy NFT contract
   const NFT = await ethers.getContractFactory('NFT');
   const nft = await NFT.deploy();
 
+  // deploy Marketplace contract
+  const Marketplace = await ethers.getContractFactory('Marketplace');
+  const marketplace = await Marketplace.deploy(1);
+
+  // console addresses
+  console.log('NFT contract address', nft.address);
+  console.log('Marketplace contract address', marketplace.address);
+
   // For each contract, pass the deployed contract and name to this function to save a copy of the contract ABI and address to the front end.
-  saveFrontendFiles(nft, "NFT");
+  saveFrontendFiles(nft, 'NFT');
+  saveFrontendFiles(marketplace, 'Marketplace');
 }
 
 function saveFrontendFiles(contract, name) {
