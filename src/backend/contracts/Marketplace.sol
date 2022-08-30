487e819d8710a89490ffedbbfa5c0f3609b504ce
Muhammad Ali <muhammad797@hotmail.com>
2022-08-30 15:32:01 +0500

    Marketplace's MakeItem & Tests

--- src/backend/contracts/Marketplace.sol
+++ src/backend/contracts/Marketplace.sol
@@ -9,8 +9,47 @@ contract Marketplace is ReentrancyGuard {
     uint256 public immutable feePercent; // fee percent on sales
     uint256 public itemCount;
 
+    struct Item {
+        uint256 itemId;
+        IERC721 nft;
+        uint256 tokenId;
+        uint256 price;
+        address payable seller;
+        bool sold;
+    }
+
+    event Offered(
+        uint256 itemId,
+        address indexed nft,
+        uint256 tokenId,
+        uint256 price,
+        address indexed seller
+    );
+
+    mapping(uint256 => Item) public items;
+
     constructor(uint256 _feePercent) {
         feeAccount = payable(msg.sender);
         feePercent = _feePercent;
+    }
+
+    function makeItem(
+        IERC721 _nft,
+        uint256 _tokenId,
+        uint256 _price
+    ) external nonReentrant {
+        require(_price > 0, "Price must be greater than zero");
+        itemCount++;
+        _nft.transferFrom(msg.sender, address(this), _tokenId);
+        items[itemCount] = Item(
+            itemCount,
+            _nft,
+            _tokenId,
+            _price,
+            payable(msg.sender),
+            false
+        );
+
+        emit Offered(itemCount, address(_nft), _tokenId, _price, msg.sender);
     }
 }
